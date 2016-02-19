e6ce30b60982765273cfe6dfb607287aac2e5d88
ssdutnowind <nowind@vip.qq.com>
2016-02-19 22:17:58 +0800

    案例动态解析

--- js/case.js
+++ js/case.js
@@ -1,5 +1,7 @@
 $(function () {
-    var caseName = 'case1';
+    var caseName = urlQuery2Obj().case;
+    if (!caseName) caseName = 'case1';
+
     $.ajax({
         type: "GET",
         url: caseName + '.html',
@@ -8,4 +10,23 @@ $(function () {
             $('#caseContainer').html(data);
         }
     });
+
+    function urlQuery2Obj() {
+        var str = location.search;
+
+        if (str[0] === '?' || str[0] === '#') {
+            str = str.substring(1);
+        }
+        var query = {};
+
+        str.replace(/\b([^&=]*)=([^&=]*)/g, function (m, a, d) {
+            if (typeof query[a] != 'undefined') {
+                query[a] += ',' + decodeURIComponent(d);
+            } else {
+                query[a] = decodeURIComponent(d);
+            }
+        });
+
+        return query;
+    }
 });

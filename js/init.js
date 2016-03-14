86c28d602b9b58879ba6ee17cba47c7fbbfe1863
ssdutnowind <nowind@vip.qq.com>
2016-03-14 21:22:57 +0800

    动态增加站长统计

--- js/init.js
+++ js/init.js
@@ -1,11 +1,41 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
+        function loadFooter() {
+            // ¶¯Ì¬¼ÓÔØfooter
+            var src, path;
+            $('script').each(function () {
+                src = $(this).attr('src');
+                if (src.indexOf('js/init.js') >= 0) {
+                    path = src;
+                }
+            });
 
-    $('.parallax').parallax();
+            if (path) {
+                var url = path.replace('js/init.js', 'template/footer.html');
+                $.ajax({
+                    type: "GET",
+                    url: url,
+                    dataType: 'html',
+                    success: function (data) {
+                        var $el = $(data);
+                        $('footer').replaceWith($el);
 
-    $('.modal-trigger').leanModal();
+                        var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+                        var cnzz = '<script src="' + cnzz_protocol + 's11.cnzz.com/z_stat.php?id=1257972001&show=pic" type="text/javascript"></script>';
+                        $el.append(cnzz);
+                    }
+                });
+            }
+        }
 
-  }); // end of document ready
+        loadFooter();
+
+        $('.button-collapse').sideNav();
+
+        $('.parallax').parallax();
+
+        $('.modal-trigger').leanModal();
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
